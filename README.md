34082e7a54c642449029bd435dce487e0abb1b58
Luke Rogers <luke.rogers@willistowerswatson.com>
2018-12-19 21:22:01 -0700

    Working example

---
+++ README.md
@@ -0,0 +1,25 @@
+# Swashbuckle Behind NGINX Reverse Proxy Example
+
+Running an ASP.NET Core WebAPI application behind a NGINX reverse proxy was challenging so I put toget her a sample in case it helps anyone else. This solution is just a 
+
+    dotnet new webapi
+
+Project with Swashbuckle added to it so you can see the ValuesController.
+
+The main piece is in the Configure method
+```csharp
+    app.UseSwagger(c => {
+        //change the path to include /api
+        c.RouteTemplate = "/api/swagger/{documentName}/swagger.json";
+    });
+
+    app.UseSwaggerUI(c =>
+    {
+        //Notice thelack of / making it relative
+        c.SwaggerEndpoint("swagger/v1/swagger.json", "My API V1");
+        //This is the reverse proxy address
+        c.RoutePrefix = "api";
+    });
+```
+
+The three comments point out the changes necessary. Check out my blog for more details https://lukerogers.com/2018/12/20/swachbuckle-behind-nginx-reverse-proxy
---
+++ src/Api/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger(c => {
+                //change the path to include /api
+                c.RouteTemplate = "/api/swagger/{documentName}/swagger.json";
+            });
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                //Notice thelack of / making it relative
+                c.SwaggerEndpoint("swagger/v1/swagger.json", "My API V1");
+                //This is the reverse proxy address
+                c.RoutePrefix = "api";
+            });
+
+            app.UseMvc();
+        }
+    }
+}
